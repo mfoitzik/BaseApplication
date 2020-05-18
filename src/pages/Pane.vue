4cf9ff5d6d6f3d941f368af42b5054cfa94ea254
mfoitzik <mike@mifo.com>
2020-05-18 15:45:41 -0700

    experiment with sass

--- src/pages/Pane.vue
+++ src/pages/Pane.vue
@@ -22,42 +22,45 @@
     >
 
       <template v-slot:before>
-        <q-page style="background-color:pink;">
-            <q-scroll-area class="q-pa-sm bg-blue-3" style="height:500px;min-height:inherit;" visible>
+        <q-page>
+            <q-scroll-area class="q-pa-sm" style="height:500px;min-height:inherit;" visible>
                 <div @contextmenu="showNodeSelected">TEST</div>
                 <div class="q-pa-md">
                   <button v-on:click = "showNodeSelected">showClickedNode</button>
-                  <q-tree
-      :nodes="customize"
-      node-key="label"
-      default-expand-all
-    >
-      <template v-slot:header-root="prop">
-        <div class="row items-center">
-          <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" class="avatar q-mr-sm">
-          <div>
-            {{ prop.node.label }}
-            <q-badge color="orange" class="q-ml-sm">New!</q-badge>
-          </div>
-        </div>
-      </template>
+                  <div class="mifo">
+                    <q-tree
+                      :nodes="customize"
+                      node-key="label"
+                      default-expand-all
+                      ref="mytree"
+                    >
+                      <template v-slot:header-root="prop">
+                        <div class="row items-center">
+                          <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" class="avatar q-mr-sm">
+                          <div>
+                            {{ prop.node.label }}
+                            <q-badge color="orange" class="q-ml-sm">New!</q-badge>
+                          </div>
+                        </div>
+                      </template>
 
-      <template v-slot:header-generic="prop">
-        <div class="row items-center">
-          <q-icon :name="prop.node.icon || 'star'" color="orange" size="28px" class="q-mr-sm" />
-          <div class="text-weight-bold text-primary">{{ prop.node.label }}</div>
-        </div>
-      </template>
+                      <template v-slot:header-generic="prop">
+                        <div class="row items-center" :id="prop.node.myid" @contextmenu="miketest" @click="mikeclick" @doubleclick="mikedouble">
+                          <q-icon :name="prop.node.icon || 'star'" color="orange" size="28px" class="q-mr-sm" />
+                          <div class="text-weight-bold text-primary">{{ prop.node.label }}</div>
+                        </div>
+                      </template>
 
-      <template v-slot:body-story="prop">
-        <span class="text-weight-thin" :id="prop.node.myid" @contextmenu="miketest">The story is: {{ prop.node.story }}</span>
-      </template>
+                      <template v-slot:body-story="prop">
+                        <span class="text-weight-thin" :id="prop.node.myid" @contextmenu="miketest">The story is: {{ prop.node.story }}</span>
+                      </template>
 
-      <template v-slot:body-toggle="prop">
-        <p class="text-caption">{{ prop.node.caption }}</p>
-        <q-toggle v-model="prop.node.enabled" label="I agree to the terms and conditions" />
-      </template>
-    </q-tree>
+                      <template v-slot:body-toggle="prop">
+                        <p class="text-caption">{{ prop.node.caption }}</p>
+                        <q-toggle v-model="prop.node.enabled" label="I agree to the terms and conditions" />
+                      </template>
+                  </q-tree>
+                </div>
                 </div>
             </q-scroll-area>
         </q-page>
@@ -75,6 +78,38 @@
       <template v-slot:before>
         <div class="q-pa-md">
           <div class="text-h4 q-mb-md">Before</div>
+          <q-tree
+                      :nodes="customize"
+                      node-key="label"
+                      default-expand-all
+                      ref="mytree"
+                    >
+                      <template v-slot:header-root="prop">
+                        <div class="row items-center">
+                          <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" class="avatar q-mr-sm">
+                          <div>
+                            {{ prop.node.label }}
+                            <q-badge color="orange" class="q-ml-sm">New!</q-badge>
+                          </div>
+                        </div>
+                      </template>
+
+                      <template v-slot:header-generic="prop">
+                        <div class="row items-center" :id="prop.node.myid" @contextmenu="miketest" @click="mikeclick" @doubleclick="mikedouble">
+                          <q-icon :name="prop.node.icon || 'star'" color="orange" size="28px" class="q-mr-sm" />
+                          <div class="text-weight-bold text-primary">{{ prop.node.label }}</div>
+                        </div>
+                      </template>
+
+                      <template v-slot:body-story="prop">
+                        <span class="text-weight-thin" :id="prop.node.myid" @contextmenu="miketest">The story is: {{ prop.node.story }}</span>
+                      </template>
+
+                      <template v-slot:body-toggle="prop">
+                        <p class="text-caption">{{ prop.node.caption }}</p>
+                        <q-toggle v-model="prop.node.enabled" label="I agree to the terms and conditions" />
+                      </template>
+                  </q-tree>
           <div v-for="n in 20" :key="n" class="q-my-md">{{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</div>
         </div>
       </template>
@@ -102,47 +137,44 @@ export default {
       splitterModel2: 50,
       customize: [
         {
-          label: 'Satisfied customers',
-          header: 'root',
+          label: 'Good food',
+          icon: 'restaurant_menu',
+          header: 'generic',
+          myid: 'gf1',
           children: [
             {
-              label: 'Good food',
-              icon: 'restaurant_menu',
+              label: 'Quality ingredients',
               header: 'generic',
-              children: [
-                {
-                  label: 'Quality ingredients',
-                  header: 'generic',
-                  body: 'story',
-                  story: 'Lorem ipsum dolor sit amet.'
-                },
-                {
-                  label: 'Good recipe',
-                  body: 'story',
-                  story: 'A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.',
-                  myid: 'mifo1'
-                }
-              ]
+              body: 'story',
+              story: 'Lorem ipsum dolor sit amet.'
             },
             {
-              label: 'Good service',
-              header: 'generic',
-              // body: 'toggle',
-              // caption: 'Why are we as consumers so captivated by stories of great customer service? Perhaps it is because...',
-              enabled: false,
-              children: [
-                { label: 'Prompt attention' },
-                { label: 'Professional waiter' }
-              ]
-            },
-            {
-              label: 'Pleasant surroundings',
-              children: [
-                { label: 'Happy atmosphere' },
-                { label: 'Good table presentation', header: 'generic' },
-                { label: 'Pleasing decor' }
-              ]
+              label: 'Good recipe',
+              body: 'story',
+              story: 'A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.',
+              myid: 'mifo1'
             }
+          ]
+        },
+        {
+          label: 'Good service',
+          header: 'generic',
+          myid: 'gs1',
+          // body: 'toggle',
+          // caption: 'Why are we as consumers so captivated by stories of great customer service? Perhaps it is because...',
+          enabled: false,
+          children: [
+            { label: 'Prompt attention' },
+            { label: 'Professional waiter' }
+          ]
+        },
+        {
+          label: 'Pleasant surroundings',
+          myid: 'ps1',
+          children: [
+            { label: 'Happy atmosphere' },
+            { label: 'Good table presentation', header: 'generic' },
+            { label: 'Pleasing decor' }
           ]
         }
       ],
@@ -153,11 +185,22 @@ export default {
   },
   methods: {
     showNodeSelected: function () {
-      console.log(this.selected)
+      this.$refs.mytree.setExpanded('Good food', false)
     },
     miketest: function () {
       event.preventDefault()
       console.log(event.currentTarget.id)
+      // JSON.stringify(this)
+    },
+    mikeclick: function () {
+      event.cancelBubble = true
+      console.log('click: ' + event.currentTarget.id)
+
+      // JSON.stringify(this)
+    },
+    mikedouble: function () {
+      // event.cancelBubble = true
+      console.log('DOUBLEclick: ' + event.currentTarget.id)
       // JSON.stringify(this)
     }
   }
